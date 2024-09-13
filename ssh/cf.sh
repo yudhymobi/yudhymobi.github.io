5bd3c5e729b5d134ae2752c0b81406980ddcb8f7
yudhymobi <92115602+yudhymobi@users.noreply.github.com>
2024-09-13 21:19:39 +0700

    Add files via upload

---
+++ ssh/cf.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Edition : Stable Edition V3.0
+# =========================================
+MYIP=$(wget -qO- ipinfo.io/ip);
+clear
+apt install jq curl -y >/dev/null 2>&1
+read -rp "Sub Domain (Contoh: yudhyganteng): " -e sub
+DOMAIN=yudhy.net
+SUB_DOMAIN=${sub}.free.yudhy.net
+CF_ID=yudhy.net@gmail.com
+CF_KEY=cfdcdb9ddd3b983a21b4cfdcaa4fd16a22aec
+set -euo pipefail
+IP=$(wget -qO- ifconfig.me/ip);
+echo "Updating DNS for ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+echo "Host : $SUB_DOMAIN"
+echo "IP=" >> /var/lib/kyt/ipvps.conf
+echo $SUB_DOMAIN > /etc/xray/domain
+echo $SUB_DOMAIN > /root/domain
+rm -f /root/cf.sh
